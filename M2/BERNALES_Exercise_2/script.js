e7d8b552aff92c10a8c643e43cbbe9ec116d6537
bernalesangela <163553930+bernalesangela@users.noreply.github.com>
2025-02-07 21:30:39 +0800

    add m2 files

---
+++ M2/BERNALES_Exercise_2/script.js
@@ -0,0 +1,8 @@
+const person = {
+    "name": "Bobby",
+    "age": 29,
+    "hobbies": ["Running", "Swimming", "Hiking"]
+};
+
+const jsonString = JSON.stringify(person);
+console.log(jsonString);
---
+++ M2/HandsOn/script.js
@@ -0,0 +1,25 @@
+function fetchCatImage() {
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "x-api-key": "live_YUo8n60S6iVaz9rnc120vzz8harWefVh64FdjwhoHSl69Izw24bSuhiC1r9kv8Dz"
+    });
+    
+    const limit = document.getElementById("limit").value;
+    var requestOptions = {
+        method: 'GET',
+        headers: headers,
+        redirect: 'follow'
+    };
+    
+    fetch(`https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=${limit}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            if (result.length > 0) {
+                document.getElementById("cat-container").innerHTML = result.map(cat => `<img src="${cat.url}" alt="Random Cat">`).join('');
+            }
+        })
+        .catch(error => console.log('error', error));
+}
+
+// Load an initial cat image on page load
+fetchCatImage();
